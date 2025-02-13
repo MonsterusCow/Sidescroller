a83b7d53e02180abeaab5e1de3cf68b9db59b578
ryan stark <ryan@starkfamily.org>
2025-02-13 07:30:05 -0600

    Initial Commit

---
+++ SidescrollerTests/SidescrollerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SidescrollerTests.swift
+//  SidescrollerTests
+//
+//  Created by RYAN STARK on 2/13/25.
+//
+
+import Testing
+@testable import Sidescroller
+
+struct SidescrollerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
