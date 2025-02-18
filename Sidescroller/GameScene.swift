56b39b081085f89867bfa20961e74c91d82f01a7
ryan stark <ryan@starkfamily.org>
2025-02-18 08:11:03 -0600

    everything

--- Sidescroller/GameScene.swift
+++ Sidescroller/GameScene.swift
@@ -8,81 +8,55 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var ball: SKSpriteNode!
+    let cam = SKCameraNode()
+    
+    var onFloor = false
+    
+    var control: GameViewController!
     
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        physicsWorld.contactDelegate = self
+        self.camera = cam
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        control = scene as? GameViewController
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.node?.name == "floor" || contact.bodyB.node?.name == "floor"{
+            print("collision with floor")
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        if contact.bodyA.node?.name == "coin" && contact.bodyB.node?.name == "ball"{
+            print("coin")
+        } else if contact.bodyB.node?.name == "coin" && contact.bodyA.node?.name == "ball"{
+            print("coin")
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+    func didEnd(_ contact: SKPhysicsContact) {
+        if contact.bodyA.node?.name == "cloud"{
+            contact.bodyA.node?.isHidden = true
+            contact.bodyA.node?.physicsBody?.categoryBitMask = 0
+        } else if contact.bodyB.node?.name == "cloud"{
+            contact.bodyB.node?.isHidden = true
+            contact.bodyB.node?.physicsBody?.categoryBitMask = 0
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    
+    func jump(){
+        ball.physicsBody?.velocity.dy = 600
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        cam.position = ball.position
     }
 }
--- Sidescroller/GameViewController.swift
+++ Sidescroller/GameViewController.swift
@@ -11,14 +11,21 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    var play: GameScene!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+                    UIDevice.current.setValue(value, forKey: "orientation")
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
+                
+                play = scene as? GameScene
                 
                 // Present the scene
                 view.presentScene(scene)
@@ -33,7 +40,7 @@ class GameViewController: UIViewController {
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
+            return .landscapeRight
         } else {
             return .all
         }
@@ -42,4 +49,27 @@ class GameViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    @IBAction func jump(_ sender: Any) {
+        play.ball.physicsBody?.velocity = CGVector(dx: Int((play.ball.physicsBody?.velocity.dx)!), dy: 1000)
+    }
+    @IBAction func right(_ sender: Any) {
+        play.ball.physicsBody?.velocity = CGVector(dx: 750, dy: Int((play.ball.physicsBody?.velocity.dy)!))
+        play.ball.texture = SKTexture(image: UIImage(named: "right")!)
+    }
+    @IBAction func down(_ sender: Any) {
+    }
+    @IBAction func left(_ sender: Any) {
+        play.ball.physicsBody?.velocity = CGVector(dx: -750, dy: Int((play.ball.physicsBody?.velocity.dy)!))
+        play.ball.texture = SKTexture(image: UIImage(named: "left")!)
+    }
+    @IBAction func up(_ sender: Any) {
+    }
+    @IBAction func stopX(_ sender: Any){
+        play.ball.physicsBody?.velocity = CGVector(dx: 0, dy: Int((play.ball.physicsBody?.velocity.dy)!))
+    }
+    @IBAction func stopY(_ sender: Any){
+        play.ball.physicsBody?.velocity = CGVector(dx: Int((play.ball.physicsBody?.velocity.dx)!), dy: 0)
+    }
+    
 }
